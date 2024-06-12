ba141fef71f7610a8f52e3c5d7928344b189462c
Robin Rausch <robin.rausch@kabelbw.de>
2024-06-12 14:07:17 +0200

    implementation of database

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from PIL import Image, ImageOps
 from io import BytesIO
-from flask import Flask, render_template, request, send_file, url_for
+from flask import Flask, render_template, request, url_for
 import base64
 import os
 from sigma.pix2pixSingle import Sigma
 import tensorflow as tf
+from database import ImageDatabase
 
 
 app = Flask(__name__)
+database = ImageDatabase("images.db")
 
 @app.route("/")
 def index():
@@ -27,7 +29,7 @@ def process_image():
         except (IndexError, ValueError):
             return "Invalid image data format", 400
 
-        temp_img_path = os.path.join("static", "drawing.png")
+        temp_img_path = os.path.join("static", "drawing.jpg")
         with Image.open(BytesIO(image_bytes)) as img:
             # make transparent color white 
             img = img.convert("RGBA")
@@ -48,7 +50,6 @@ def process_image():
             img.save(temp_img_path)
             print("Image saved")
 
-
         # Perform image processing here
         sigma = Sigma()
         generator = sigma.generator()
@@ -57,7 +58,10 @@ def process_image():
         print("save checkpoint")
         checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer, generator=generator)
         checkpoint_dir = 'sigma/training_checkpoints-cars'
-        checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+        status = checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+
+        # Optionally, use status.assert_consumed() to ensure all variables are loaded, if you want strict checking.
+        status.expect_partial()
 
         print("generate image")
         img_name = "drawing"
@@ -71,14 +75,24 @@ def process_image():
             print("generated image saved")
 
         # Create URLs for the before and after images
-        before_image_url = url_for("static", filename="drawing.png")
+        before_image_url = url_for("static", filename="drawing.jpg")
         after_image_url = url_for("static", filename="result_drawing.jpg")
+
+        database.add_images_to_database(open(temp_img_path, "rb").read(), open(output_path, "rb").read())
 
         return render_template("result.html", before_image=before_image_url, after_image=after_image_url, download_url=after_image_url)
 
-
     except Exception as e:
         return str(e), 500
+    
+@app.route("/rateResults/<int:rating>", methods=["GET"])
+def rate_results(rating):
+    # Create URLs for the before and after images
+    before_image_url = url_for("static", filename="drawing.jpg")
+    after_image_url = url_for("static", filename="result_drawing.jpg")
+    
+    database.add_rating(rating, open("static/drawing.jpg", "rb").read(), open("static/result_drawing.jpg", "rb").read())
+    return render_template("result.html", before_image=before_image_url, after_image=after_image_url, download_url=after_image_url)
 
 if __name__ == "__main__":
     app.run(debug=True)
